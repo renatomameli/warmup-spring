5ebef1159128b0c40210ed6af2ab9226a72964e7
Renato Mameli <renato.mameli@teamviewer.com>
2025-08-16 22:27:46 +0200

    add local publishing, clean dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,21 @@
 plugins {
     kotlin("jvm") version "2.1.20"
+    `java-library`
+    `maven-publish`
 }
 
 group = "com.mameli"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    mavenLocal()
     mavenCentral()
 }
 
 dependencies {
-    implementation(platform("org.springframework.boot:spring-boot-dependencies:3.5.2"))
+    compileOnly(platform("org.springframework.boot:spring-boot-dependencies:3.5.2"))
 
-    implementation("org.springframework:spring-web")
-    implementation("org.springframework.boot:spring-boot")
+    compileOnly("org.springframework:spring-web")
     implementation("org.springframework.boot:spring-boot-autoconfigure")
 
     implementation("org.slf4j:slf4j-api")
@@ -24,6 +26,25 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(21)
+}
+
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            groupId = group.toString()
+            artifactId = "warmup"
+            version = version.toString()
+        }
+    }
+    repositories {
+        maven {
+            name = "local"
+            url = uri("${layout.buildDirectory}/repo")
+        }
+    }
 }
